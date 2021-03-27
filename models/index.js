f7a94edba9e672a7ea21870ddd8f41e28d80186c
SunilDeveloper7 <sunnil474@gmail.com>
2021-03-27 13:00:30 -0700

    config for deploy

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // require mongoose package
 const mongoose = require('mongoose')
-
+if(process.env.NODE_ENV === 'development') {
 // mongoose config
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/project-three'
 
@@ -21,3 +21,40 @@ console.log(`⛓ mongoDB connection @ ${db.host}:${db.port}`)
 db.on('error', err => {
 console.error(`🔥 something has gone wrong with the DB!!!!\n ${err}`)
 })
+}else {
+  
+// mongoose config
+//userName-hello_users
+//password-sunillama
+
+  const MongoClient = require('mongodb').MongoClient;
+
+  const uri = process.env.ATLAS_URL
+
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect(err => {
+
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+  mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+
+  const db = mongoose.connection;
+
+  // db methods for debug
+  db.once('open', () => {
+    console.log(`⛓ mongoDB connection @ ${db.host}:${db.port}`)
+  })
+
+  db.on('error', err => {
+    console.error(`🔥 something has gone wrong with the DB!!!!\n ${err}`)
+  })
+}
